b1309fee702df766a0e3d6ad4c3b4f914e8fa108
王亚星 <funnycoderstar@gmail.com>
2017-07-20 15:09:01 +0800

    feat: promise解决异步

--- app.js
+++ app.js
@@ -1,16 +1,102 @@
-require('./data/index.js');
+const util = require('./utils/util.js');
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    // 微信授权
+    wx.getSetting({
+      success(res) {
+        if (!!res['scope.userInfo'] || !res['scope.writePhotosAlbum']) {
+          wx.authorize({
+            scope: ['scope.userInfo', 'scope.writePhotosAlbum'],
+            success() {
+              wx.getUserInfo();
+              wx.saveImageToPhotosAlbum();
+              util.checkSession()
+                .then((wechatSessionValid) => {
+                  return util.request('/auth/login', {}, 'get')
+                    .then(function (res) {
+                      const serverSessionValid = !response.data.data;
+                      return wechatSessionValid && serverSessionValid;
+                    });
+                })
+                .then(function (sessionValid) {
+                  if (sessionValid) {
+                    // session没有过期，直接获取userInfo
+                    return util.getUserInfo();
+                  }
+                  return util.login() // 执行微信登录
+                    .then(that.getLogin) // 执行剩下的登录过程
+                })
+                // 处理登录之后的一些事情
+                .then(function (userInfo) {
+                  that.globalData.hasLogin = true;
+                  const cbs = that.globalData.loginCbs;
+                  // 调用所有的回调
+                  for (let i = 0; i < cbs.length; i++) {
+                    cbs[i]();
+                  }
+                  return userInfo;
+                })
+            },
+            fail() {
+              // console.log('拒绝授权');  
+              wx.navigateTo({
+                url: '/pages/authorization/authorization'
+              });
+            },
+          })
+        }
+      }
+    })
   },
-  getUserInfo:function(cb){
+  // 执行回调
+  onLogin(cb) {
+    if (this.globalData.hasLogin) {
+      cd();
+    } else {
+      this.globalData.loginCbs.push(cb);
+    }
+  },
+  getLogin: function (loginRes) {
+    //调用登录接口
     var that = this;
-    if(this.globalData.userInfo){
+    return new Promise(function (resolve, reject) {
+      if (loginRes.code) {
+        that.globalData.code = loginRes.code;
+        console.log(that.globalData.code);
+      } else {
+        console.log('获取用户登录态失败！' + loginRes.errMsg)
+      }
+      // 需要通过调用userInfo获取信息用于登录
+      // TODO: 这里调用了userInfo，可能需要对得到的数据进行缓存
+      wx.getUserInfo({
+        success: function (res) {
+          console.log(res);
+          const params = {
+            code: that.globalData.code,
+            raw_data: res.rawData,
+            encrypted_data: res.encryptedData,
+            iv: res.iv,
+            signature: res.signature,
+          };
+          return util.request('/auth/login', params, 'POST')
+            .then(function (response) {
+              console.log(response.data, '登录info');
+              that.globalData.userInfo = res.userInfo; // 保存了
+              return resolve(res.userInfo);
+            });
+        }
+      })
+    });
+  },
+  getUserInfo: function (cb) {
+    var that = this;
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
       wx.login({
         success: function () {
@@ -24,7 +110,11 @@ App({
       });
     }
   },
-  globalData:{
-    userInfo:null
+  globalData: {
+    userInfo: null,
+    hasLogin: null, // 是否已经完成登录
+    // TODO: 可能需要考虑重复调用的问题
+    loginCbs: [], // 所有登录成功后的回调
+
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,52 +1,168 @@
-function formatTime(time) {
-  if (typeof time !== 'number' || time < 0) {
-    return time
-  }
+// function formatTime(time) {
+//   if (typeof time !== 'number' || time < 0) {
+//     return time
+//   }
 
-  var hour = parseInt(time / 3600)
-  time = time % 3600
-  var minute = parseInt(time / 60)
-  time = time % 60
-  // 这里秒钟也取整
-  var second = parseInt(time)
+//   var hour = parseInt(time / 3600)
+//   time = time % 3600
+//   var minute = parseInt(time / 60)
+//   time = time % 60
+//   // 这里秒钟也取整
+//   var second = parseInt(time)
 
-  return ([hour, minute, second]).map(function (n) {
-    n = n.toString()
-    return n[1] ? n : '0' + n
-  }).join(':')
-}
+//   return ([hour, minute, second]).map(function (n) {
+//     n = n.toString()
+//     return n[1] ? n : '0' + n
+//   }).join(':')
+// }
 
-let API_HOST = "http://xxx.com/xxx";
-let DEBUG = false;
-var Mock = require("./mock.js");
-function ajax(data = '', fn, method = "get", header = {}) {
-    if (!DEBUG) {
-        wx.request({
-            url: API_HOST + data,
-            method: method ? method : 'get',
-            data: {},
-            header: header ? header : { "Content-Type": "application/json" },
+// let API_HOST = "http://xxx.com/xxx";
+// let DEBUG = false;
+// var Mock = require("./mock.js");
+// function ajax(data = '', fn, method = "get", header = {}) {
+//     if (!DEBUG) {
+//         wx.request({
+//             url: API_HOST + data,
+//             method: method ? method : 'get',
+//             data: {},
+//             header: header ? header : { "Content-Type": "application/json" },
+//             success: function (res) {
+//                 fn(res);
+//             }
+//         });
+//     } else {
+//         // 模拟数据
+//         var res = Mock.mock({
+//             // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+//             'list|1-10': [{
+//                 // 属性 id 是一个自增数，起始值为 1，每次增 1
+//                 'id|+1': 1
+//             }]
+//         })
+//         // 输出结果
+//         console.log(JSON.stringify(res, null, 4))
+//         fn(res);
+
+//     }
+// }
+
+// module.exports = {
+//     ajax: ajax,
+//     formatTime: formatTime
+// }
+const apiUrl = '';
+function getStorage(key) {
+    return new Promise(function (resolve, reject) {
+        wx.getStorage({
+            key: key,
             success: function (res) {
-                fn(res);
-            }
+                resolve(res.data);
+            },
+            fail: function (res) {
+                if (res.errMsg == 'getStorage:fail data not found') {
+                    resolve(null);
+                } else {
+                    reject(res.errMsg);
+                }
+            },
         });
-    } else {
-        // 模拟数据
-        var res = Mock.mock({
-            // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-            'list|1-10': [{
-                // 属性 id 是一个自增数，起始值为 1，每次增 1
-                'id|+1': 1
-            }]
+    })
+};
+function setStorage(key, value) {
+    return new Promise(function (resolve, reject) {
+        wx.setStorage({
+            key: key,
+            data: value,
+            success: function (res) {
+                // TODO: 不知道返回什么
+                resolve(res.data);
+            },
+            fail: function (res) {
+                reject(res.errMsg);
+            },
         })
-        // 输出结果
-        console.log(JSON.stringify(res, null, 4))
-        fn(res);
-
-    }
+    })
+};
+function checkSession() {
+    return new Promise(function (resolve, reject) {
+        wx.checkSession({
+            success: function () {
+                resolve(true);
+            },
+            fail: function () {
+                resolve(false);
+            },
+        });
+    })
+}
+function login() {
+    return new Promise(function (resolve, reject) {
+        wx.login({
+            success: function (res) {
+                resolve(res);
+            },
+            fail: function (res) {
+                reject(res.errMsg);
+            },
+        });
+    })
 }
 
-module.exports = {
-    ajax: ajax,
-    formatTime: formatTime
-}
+function request() {
+    return getStorage('cookie')
+        // 请求数据
+        .then(function (cookie) {
+            return new Promise(function (resolve, reject) {
+                // 请求api时带上cookie
+                wx.request({
+                    url: `${apiUrl}${url}`,
+                    data: data,
+                    method: method,
+                    header: {
+                        cookie: cookie, // 可能没有数据
+                    },
+                    success: function (res) {
+                        console.log("success", method);
+                        // 弹窗提示错误
+                        if (res.data.status !== 0) {
+                            wx.showToast({
+                                title: res.data.message ? res.data.message : 'error',
+                                duration: 2000,
+                            });
+                        } else {
+                            resolve(res);
+                        }
+                    },
+                    fail: function (res) {
+                        console.log(res, '错误数据');
+                        reject(res);
+                    },
+                });
+            })
+        })
+        .then(function (res) {
+            return setStorage('cookie', response.header['Set-Cookie'].split(';')[0])
+                .then(function () {
+                    return response;
+                })
+        })
+        .catch(function (errMes) {
+            wx.showToast({
+                title: errMsg || 'error',
+                duration: 2000,
+            });
+
+        })
+};
+function getData(url, data, method) {
+    return request(url, data, method)
+        .then(function (response) {
+            return response.data;
+        })
+};
+exports.request = request;
+exports.getData = getData;
+exports.apiUrl = apiUrl;
+exports.checkSession = checkSession;
+exports.login = login;
+exports.getUserInfo = getUserInfo;
